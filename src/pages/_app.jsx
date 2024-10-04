9230d22f03c6e078c051d40366e4fb056238df26
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-10-04 16:13:05 +0300

    added page favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,16 +1,23 @@
+import Head from 'next/head';
 import Header from '@/components/Header';
 import Footer from '../components/Footer';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="flex flex-col min-h-screen"> 
+    <>
+      <Head>
+        <link rel="icon" href="/news.png" /> 
+      </Head>
+      <div className="flex flex-col min-h-screen"> 
       <Header/>
       <main className="flex-grow">
         <Component {...pageProps} />
       </main>
       <Footer />
     </div>
+    </>
+
   );
 }
 
